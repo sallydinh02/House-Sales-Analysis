081f2dd0590b989e4306c17309f9f0487ecbee64
sallydinh02 <sallymyky02@gmail.com>
2024-10-19 13:38:16 -0700

    Update kchousesales.sql

--- kchousesales.sql
+++ kchousesales.sql
@@ -1,3 +1,8 @@
+USE HouseSales
+SELECT * FROM KCHouseFinal
+
 SELECT AVG(price), condition FROM KCHouseFinal
 GROUP BY condition
---SELECT * FROM KCHouseFinal
+
+SELECT AVG(price), [view] FROM KCHouseFinal
+GROUP BY [view]
